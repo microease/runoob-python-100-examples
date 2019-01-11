59852c45393a317f71a0dc4598527890935bf8e0
浮生若梦 <microease@163.com>
2019-01-11 13:59:38 +0800

    add

---
+++ 011ok.py
@@ -0,0 +1,13 @@
+# 题目：古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？
+# 斐波那契数列
+
+def fib(n):
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [1, 1]
+    fibs = [1, 1]
+    for i in range(2, n):
+        fibs.append(fibs[-1] + fibs[-2])
+    return fibs
+print(fib(22))
