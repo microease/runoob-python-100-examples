331de3a0cd25a0ad5128bcbac7dc5f2dbe60b19b
浮生若梦 <microease@163.com>
2019-01-15 16:15:38 +0800

    add

--- 014.py
+++ 014.py
@@ -2,5 +2,13 @@
 # coding=utf-8
 
 
-x = int(input("请输入一个数字："))
+x = int(input("请输入一个正整数："))
+temp = []
+while x != 1:
+    for i in range(2, x + 1):
+        if x % i == 0:
+            temp.append(i)
+            x = x / i
+            break
 
+print(temp)
